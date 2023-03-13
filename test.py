cbc6fdbdc91a1f88738f0acb059b1e2e178ae117
Ovlic <74332950+Ovlic@users.noreply.github.com>
2023-03-13 10:06:21 -0700

    testing stuff

--- test.py
+++ test.py
@@ -1,39 +1,22 @@
-"""
-from datetime import datetime
+from rich.console import Console
+from rich.layout import Layout
 
-text = "Single tracking at Sunnyvale and Lawrence until 3:45pm. Trains will board on northbound platform."
+console = Console()
+layout = Layout()
 
+# Divide the "screen" in to three parts
+layout.split(
+    Layout(name="header", size=3),
+    Layout(ratio=1, name="main"),
+    Layout(size=10, name="footer"),
+)
+# Divide the "main" layout in to "side" and "body"
+layout["main"].split_row(
+    Layout(name="side"),
+    Layout(name="body", ratio=2),
+    #direction="horizontal"
+)
+# Divide the "side" layout in to two
+layout["side"].split(Layout(), Layout())
 
-if "am" in text.lower() or "pm" in text.lower():
-    # Time, not date
-    date = datetime.now()
-    if ":" in text:
-        hours = int(text.split('until')[1].split(':')[0])
-        if "pm" in text.lower():
-            hours += 12
-        mins = int(text.split(':')[1].split('am' if 'am' in text.lower() else 'pm')[0])
-        new_period = date.replace(hour=hours, minute=mins).strftime('%Y-%m-%d-%H:%M')
-        print(new_period)
-
-text2 = "Single tracking at 22nd St and Bayshore beginning with NB407, all trains will board on southbound platform until 4pm."
-date = datetime.now()
-hours = int(text2.split('until')[1].split('.')[0].split('pm' if 'pm' in text2.lower() else 'am')[0])
-if "pm" in text2.lower():
-    hours += 12
-new_period2 = date.replace(hour=hours, minute=00).strftime('%Y-%m-%d-%H:%M')
-print(new_period2)"""
-
-"""import requests
-resp = requests.head("https://t.co/EW9KVx2yka")
-print(resp.status_code)
-print(resp.headers["Location"])"""
-
-"""t = []
-print("none" if t is None else "not none")
-print("none" if t is [] else "not none")
-print("none" if not t else "not none")"""
-
-text = "southbound is in this text but not the opposite!"
-print(text.replace("NBD", "northbound"))
-text.replace("southbound", "haha")
-print(text)
+console.print(layout)
